943be701df4633ab915908d3f4e89031b27f0bee
irobust <p_permpimon@hotmail.com>
2025-09-30 10:26:42 +0700

    Update bootstrap.sh

--- 02-Beat-Logstash/bootstrap.sh
+++ 02-Beat-Logstash/bootstrap.sh
@@ -1,7 +1,8 @@
 echo "[Task 1] Add GPG key from Elastic and app package to source list"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+echo "deb https://artifacts.elastic.co/packages/9.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-9.x.list
+echo "deb https://artifacts.elastic.co/packages/oss-9.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-9.x.list
 
 echo "[Task 2] Update system"
 sudo apt-get update
